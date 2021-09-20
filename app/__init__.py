b3a937d5da07732ff5f9f1ddc69baf5bc7c7d9eb
hope-mbuthye38 <-C>
2021-09-20 10:50:01 +0300

    changes in imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,38 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import config_options
-
-app = Flask(__name__)
-bootstrap = Bootstrap()
+from config import DevConfig, config_options
 
 def create_app(config_name):
 
+    app = Flask(__name__)
 
-    # Creating the app configurations
+    # creating the app configurations
     app.config.from_object(config_options[config_name])
 
-    # Initializing flask extensions
+    # initializing flask extensions
     bootstrap.init_app(app)
 
-    # Will add the views and forms
+    # register
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-
- 
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
     return app
 
-    
+
+
+app = Flask(__name__, instance_relative_config= True)
+bootstrap = Bootstrap()
+
+ # Creating the app configurations
+app.config.from_object(DevConfig)
+
+    # Initializing flask extensions
+bootstrap = Bootstrap(app)
+
+from .main import views    
    
 
