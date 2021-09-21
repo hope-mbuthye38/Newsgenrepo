a6654e8401d1b716ce33b2e12447f830fe1b72d3
hope-mbuthye38 <-C>
2021-09-21 08:58:47 +0300

    url implementation

--- config.py
+++ config.py
@@ -1,21 +1,32 @@
 import os
 
+# from flask import config
 class Config:
-
-    SECRET_KEY ='5578890022345789'
-    NEWS_ARTICLE_API='https://newsapi.org/v2/everything?source={}&apiKey=55eeed8e67e949608a10479d38e11f96'
-    API_KEY ='55eeed8e67e949608a10479d38e11f96'
-    NEWS_SOURCE_API ='https://newsapi.org/v2/sources?apiKey=55eeed8e67e949608a10479d38e11f96'
-
+    '''
+    General configuration parent class
+    '''
+    ARTICLE_API_BASE_URL ='https://newsapi.org/v2/everything?sources={}&apiKey=55eeed8e67e949608a10479d38e11f96'
+    NEWS_SOURCES='https://newsapi.org/v2/sources?apiKey=55eeed8e67e949608a10479d38e11f96'
 
 class ProdConfig(Config):
+    '''
+    Production configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
     pass
 
-
 class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
+ 
     DEBUG = True
 
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
+    'development':DevConfig,
+    'production':ProdConfig
+ }
+  
