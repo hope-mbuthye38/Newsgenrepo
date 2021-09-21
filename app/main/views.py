d9a6e23ec6a8af8c87ac7879accc353d749169e8
hope-mbuthye38 <-C>
2021-09-21 09:52:24 +0300

    sources url

--- app/main/views.py
+++ app/main/views.py
@@ -4,16 +4,16 @@ from ..requests import get_news,get_article
 @app.route('/')
 def index():
     '''
-    View root page function that returns the index page and its data
+    View root page function that returns the index page and its content
     '''
     news_sources = get_news()
     # news_article = get_article()
-    title = 'Home - Welcome to the best news Article Website online'
-    return render_template('index.html',title = title, new_sources = news_sources)
+    title = 'Home - launch into the best news Article Website online'
+    return render_template('index.html',title = title, sources = news_sources)
 @app.route('/news/<string:new_id>')
 def news(new_id):
     '''
-    View news page function that returns the news details page and its data
+    View news page function that returns the news details page and all its content
     '''
     news = get_article(new_id)
     print('----------------')
