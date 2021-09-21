f5bc33e6c6af1989a124a9d306e998f260d3e793
hope-mbuthye38 <-C>
2021-09-21 09:01:07 +0300

    server imports

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 from app import create_app
-from app import app
+from flask_script import Manager,Server
 
 # Creating app instance
 app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server',Server)
 
 if __name__ == '__main__':
-    app.run(debug =True)
+    manager.run()
 
